34078c26d5d80132689da202eb84556366ed8195
xinyang <41963211+xinyang-go@users.noreply.github.com>
2025-11-29 13:03:31 +0800

    reg.zig support read/write only mode

--- src/core/reg.zig
+++ src/core/reg.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn Reg(comptime T: type) type {
+pub fn RW(comptime T: type) type {
     comptime {
         std.debug.assert(@typeInfo(T) == .@"struct");
         std.debug.assert(@typeInfo(T).@"struct".layout == .@"packed");
@@ -8,21 +8,55 @@ pub fn Reg(comptime T: type) type {
     return struct {
         ptr: *allowzero volatile @typeInfo(T).@"struct".backing_integer.?,
         const Self = @This();
-        pub fn init(addr: usize) Self {
+        pub inline fn init(addr: usize) Self {
             return .{ .ptr = @ptrFromInt(addr) };
         }
-        pub fn write(self: Self, value: T) void {
+        pub inline fn write(self: Self, value: T) void {
             self.ptr.* = @bitCast(value);
         }
-        pub fn read(self: Self) T {
+        pub inline fn read(self: Self) T {
             return @bitCast(self.ptr.*);
         }
-        pub fn modify(self: Self, fields: anytype) void {
+        pub inline fn modify(self: Self, fields: anytype) void {
             var value = self.read();
             const info = @typeInfo(@TypeOf(fields)).@"struct";
             inline for (info.fields) |field| {
                 @field(value, field.name) = @field(fields, field.name);
             }
+        }
+    };
+}
+
+pub fn RO(comptime T: type) type {
+    comptime {
+        std.debug.assert(@typeInfo(T) == .@"struct");
+        std.debug.assert(@typeInfo(T).@"struct".layout == .@"packed");
+    }
+    return struct {
+        ptr: *allowzero volatile @typeInfo(T).@"struct".backing_integer.?,
+        const Self = @This();
+        pub inline fn init(addr: usize) Self {
+            return .{ .ptr = @ptrFromInt(addr) };
+        }
+        pub inline fn read(self: Self) T {
+            return @bitCast(self.ptr.*);
+        }
+    };
+}
+
+pub fn WO(comptime T: type) type {
+    comptime {
+        std.debug.assert(@typeInfo(T) == .@"struct");
+        std.debug.assert(@typeInfo(T).@"struct".layout == .@"packed");
+    }
+    return struct {
+        ptr: *allowzero volatile @typeInfo(T).@"struct".backing_integer.?,
+        const Self = @This();
+        pub inline fn init(addr: usize) Self {
+            return .{ .ptr = @ptrFromInt(addr) };
+        }
+        pub inline fn write(self: Self, value: T) void {
+            self.ptr.* = @bitCast(value);
         }
     };
 }
